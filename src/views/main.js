24e0c36e0ba3b3aeac498ecc8550f927f1c9aa2e
ndoubleu <65906953+ndoubleu@users.noreply.github.com>
2022-03-03 19:04:19 +0300

    Update main.js

--- src/views/main.js
+++ src/views/main.js
@@ -6,15 +6,42 @@ class MainPage extends React.Component
     constructor(props) {
         super(props);
         this.state = {
+            data:[],
         };
     }
     async componentDidMount(){
-
+        var url = 'https://jsonplaceholder.typicode.com/posts/1/comments';
+        const response = await fetch(url, {
+            method : 'get',
+            headers: new Headers({
+                'Content-Type': 'application/json'            
+            })
+        });
+        const data = await response.json();
+        if(response.status===200){
+            this.setState({data:data});
+        }
     }
     render(){
+        var {data} = this.state;
         return(
-            <div className='block py-4'>
-
+            <div className='block container py-4'>
+                <div className='d-flex flex-column mt-4'>
+                {
+                    data.map(comment => (
+                        <div className='d-flex flex-column cart' key={comment.id}>
+                            <div className='d-flex flex-nowrap cart-title'>
+                                <span className='text bold mr-4'>{comment.name}</span>
+                                <span className='text opacity'>{comment.email}</span>
+                            </div>
+                            <div className='cart-body'>
+                                <GoTriangleUp className='cart-arrow'/>
+                                {comment.body}
+                            </div>
+                        </div>
+                    ))
+                }
+                </div>
             </div>
         )
     }
